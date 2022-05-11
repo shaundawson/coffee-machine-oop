fdf28a527b8a32ae9832b18fc3b5efc2d8542be4
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-05-11 18:58:57 -0400

    turtle

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 # from another_module import another_variable
 # print(another_variable)
 
-from turtle import Turtle
+from turtle import Screen, Turtle
 
 shaun = Turtle()
 print(shaun)
+
+my_screen = Screen
+print(my_screen.canvheight)
+my_screen.exitonclick()
