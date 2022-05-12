c79957d6426fdf8544e1e68e32a887d9ffd2df9f
Shaun Dawson <23201454+shaundawson@users.noreply.github.com>
2022-05-11 20:19:33 -0400

    solution

--- main.py
+++ main.py
@@ -1,9 +1,34 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+from prettytable import PrettyTable
 
-# TODO Print report.
-# TODO Check resources sufficient?
-# TODO Process Coins
-# TODO Check transaction successful?
-# TODO Make Coffee.
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+menu = Menu()
+is_on = True
+
+
+while is_on:
+    options = menu.get_items()
+    choice = input(f"What would you like? {options}")
+    if choice == "off":
+        is_on = False
+    # TODO Print report.
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        # TODO Check resources sufficient?
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink):
+            # TODO Check transaction successful?
+            if money_machine.make_payment(drink.cost):
+                # TODO Make Coffee.
+                coffee_maker.make_coffee(drink)
+
+
+
+
+
+
